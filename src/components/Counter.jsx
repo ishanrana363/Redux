1ef158d0d965fe1e448941b3d20331a6dd9ed82c
ishanrana363 <ishanrana363@gmail.com>
2025-01-06 07:31:13 +0600

    increment and decrement added

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from '../redux/features/counter/counterSlice';
 
 const Counter = () => {
-    const count = useSelector((state)=>state.counter.value)
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    if(count>0){
+      dispatch(decrement());
+    }
+  };
+
   return (
     <div>
-        <h1>ishan</h1>
-      {
-        count
-      }
+      <div className="h-screen flex justify-center items-center">
+        <button
+          onClick={handleIncrement}
+          className="bg-green-500 px-3 py-[6px] rounded-md mr-3"
+        >
+          +
+        </button>
+        {count}
+        <button onClick={handleDecrement} className="bg-green-500 px-3 py-[6px] rounded-md ml-3">
+          -
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
